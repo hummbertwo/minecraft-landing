dcc2252aae4321d4a7760a6e83244144e4d0e9cf
hummbertwo <65779926+hummbertwo@users.noreply.github.com>
2025-08-28 19:12:12 -0600

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,10 +1,9 @@
-import util from "minecraft-server-util";
-
-export default async function handler(req, res) {
-  try {
-    const result = await util.status("smpcremaserver.duckdns.org", 25565);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500).json({ online: false });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    online: true,
+    players: [
+      { name: "Steve", uuid: "123" },
+      { name: "Alex", uuid: "456" }
+    ]
+  });
 }
